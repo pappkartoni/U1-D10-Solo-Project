407d4a727eabff6896d7124e737942f0df12e919
Florian Dreyer <flodreyer@web.de>
2022-12-02 12:35:37 +0100

    solved it, maybe finetune it a little later

---
+++ homework.js
@@ -0,0 +1,781 @@
+/* This movies array is used throughout the exercises. You’re not supposed to alter it. */
+
+const movies = [
+
+    {
+
+    Title: "The Lord of the Rings: The Fellowship of the Ring",
+
+    Year: "2001",
+
+    imdbID: "tt0120737",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "The Lord of the Rings: The Return of the King",
+
+    Year: "2003",
+
+    imdbID: "tt0167260",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "The Lord of the Rings: The Two Towers",
+
+    Year: "2002",
+
+    imdbID: "tt0167261",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "Lord of War",
+
+    Year: "2005",
+
+    imdbID: "tt0399295",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "Lords of Dogtown",
+
+    Year: "2005",
+
+    imdbID: "tt0355702",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BNDBhNGJlOTAtM2ExNi00NmEzLWFmZTQtYTZhYTRlNjJjODhmXkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "The Lord of the Rings",
+
+    Year: "1978",
+
+    imdbID: "tt0077869",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "Lord of the Flies",
+
+    Year: "1990",
+
+    imdbID: "tt0100054",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BOTI2NTQyODk0M15BMl5BanBnXkFtZTcwNTQ3NDk0NA@@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "The Lords of Salem",
+
+    Year: "2012",
+
+    imdbID: "tt1731697",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BMjA2NTc5Njc4MV5BMl5BanBnXkFtZTcwNTYzMTcwOQ@@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "Greystoke: The Legend of Tarzan, Lord of the Apes",
+
+    Year: "1984",
+
+    imdbID: "tt0087365",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BMTM5MzcwOTg4MF5BMl5BanBnXkFtZTgwOTQwMzQxMDE@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "Lord of the Flies",
+
+    Year: "1963",
+
+    imdbID: "tt0057261",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BOGEwYTlhMTgtODBlNC00ZjgzLTk1ZmEtNmNkMTEwYTZiM2Y0XkEyXkFqcGdeQXVyMzU4Nzk4MDI@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "The Avengers",
+
+    Year: "2012",
+
+    imdbID: "tt0848228",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "Avengers: Infinity War",
+
+    Year: "2018",
+
+    imdbID: "tt4154756",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "Avengers: Age of Ultron",
+
+    Year: "2015",
+
+    imdbID: "tt2395427",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BMTM4OGJmNWMtOTM4Ni00NTE3LTg3MDItZmQxYjc4N2JhNmUxXkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg",
+
+    },
+
+    {
+
+    Title: "Avengers: Endgame",
+
+    Year: "2019",
+
+    imdbID: "tt4154796",
+
+    Type: "movie",
+
+    Poster:
+
+    "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
+
+    },
+
+    ];
+
+/*
+
+ASSIGNMENT RULES
+
+– All the answers must be written in JavaScript
+
+– You can ask for help, reach the Teaching Assistants if needed
+
+– You can Google / use StackOverflow BUT only when you think you need something we didn’t cover during lessons yet
+
+– You can test your code in a separate file or de-commenting the single exercises in this one.
+
+– You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you’re using macOS or Linux.
+
+– Complete as many exercises as you can
+
+*/
+
+// JS Basics
+
+/* EXERCISE A
+
+Create a variable called test and assign a string value to it.
+
+*/
+
+let test = "test";
+
+/* EXERCISE B
+
+Create a variable called sum and assign to it the result of the sum between the numbers 10 and 20.
+
+*/
+
+let sum = 10 + 20;
+
+/* EXERCISE C
+
+Create a variable called random and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
+
+*/
+
+let random = Math.random() * 20;
+let randomInt = Math.floor(Math.random() * 21); // this what you really wanted right?
+
+/* EXERCISE D
+
+Create a variable called me and assign to it an object containing the following information: name = your name, surname = your surname, age = your age.
+
+*/
+
+let me = {name: "Florian", surname: "Dreyer", age: 30};
+
+/* EXERCISE E
+
+Write a piece of code for programmatically removing the age property from the previously create object.
+
+*/
+
+delete me.age;
+
+/* EXERCISE F
+
+Write a piece of code for programmatically adding to the me object you defined before an array called skills, containing the programming languages you know right now.
+
+*/
+
+me.skills = ["java", "js", "css", "html", "python", "c", "scala", "matlab"];
+
+/* EXERCISE G
+
+Write a piece of code for programmatically removing the last skill from the skills array inside the me object.
+
+*/
+
+me.skills.pop();
+
+// JS Functions
+
+/* EXERCISE 1
+
+Write a function called dice; it should randomize an integer number between 1 and 6.
+
+*/
+
+function dice() {
+    return Math.floor(Math.random() * 6) + 1; //yikes
+}
+
+console.log("\nExercise 1");
+console.log(dice());
+console.log(dice());
+console.log(dice());
+console.log(dice());
+
+/* EXERCISE 2
+
+Write a function called whoIsBigger which receives 2 numbers as parameters and returns the biggest one.
+
+*/
+
+function whoIsBigger(x,y) {
+    return (x >= y) ? x : y; // if same return any one of them, i chose x
+}
+
+console.log("\nExercise 2");
+console.log(whoIsBigger(3,4));
+console.log(whoIsBigger(5,4));
+console.log(whoIsBigger(4,4));
+
+/* EXERCISE 3
+
+Write a function called splitMe which receives a string as a parameter and returns an array with every word in that string.
+
+Ex.: splitMe("I love coding") => returns ["I", "Love", "Coding"]
+
+*/
+
+function splitMe(str) {
+    return str.split(" ");
+}
+
+console.log("\nExercise 3");
+console.log(splitMe("I love coding"));
+
+/* EXERCISE 4
+
+Write a function called deleteOne which receives a string and a boolean as parameters.
+
+If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
+
+*/
+
+function deleteOne(str, bool) {
+    return bool ? str.slice(1) : str.slice(0,-1);
+}
+
+console.log("\nExercise 4");
+console.log(deleteOne("hello", true));
+console.log(deleteOne("hello", false));
+
+/* EXERCISE 5
+
+Write a function called onlyLetters which receives a string as a parameter and returns it removing all the digits.
+
+Ex.: onlyLetters("I have 4 dogs") => returns "I have dogs"
+
+*/
+
+function onlyLetters(str) {
+    let splits = str.split(" ");
+    for (let i = 0; i < splits.length; i++) {
+        splits[i] = splits[i].replace(/\d+/g,'');
+        if (splits[i] === "") {
+            splits.splice(i, 1);
+        }
+    }
+    return splits.join(" ");
+}
+
+console.log("\nExercise 5");
+console.log(onlyLetters("I have 4 dogs"));
+console.log(onlyLetters("HTML5 is certainly something"));
+
+/* EXERCISE 6
+
+Write a function called isThisAnEmail which receives a string as a parameter and returns true if the string is a valid email address.
+
+*/
+
+function isThisAnEmail(str) {
+    return (/^[a-zA-Z]+[a-zA-Z0-9_.]+@[a-zA-Z.]+[a-zA-Z]$/).test(str); //this was suggested to me by the group
+}
+
+console.log("\nExercise 6");
+console.log(isThisAnEmail("me@something.com"));
+console.log(isThisAnEmail("mayonnaise"));
+
+/* EXERCISE 7
+
+Write a function called whatDayIsIt that should return the current day of the week.
+
+*/
+
+function whatDayIsIt() {
+    let today = new Date();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    return days[today.getDay()];
+}
+
+console.log("\nExercise 7");
+console.log(whatDayIsIt());
+
+/* EXERCISE 8
+
+Write a function called rollTheDices which receives a number as a parameter.
+
+It should invoke the dice() function defined in Ex1 the specified amount of times,
+
+and return an object containing a sum property holding the sum of all values extracted
+
+and a values array containing the single values of the dicerolls themselves.
+
+Example: RollTheDices(3) => returns {
+
+sum: 10
+
+values: [3, 3, 4]
+
+}
+
+*/
+
+function rollTheDices(n) {
+    let i = 0;
+    let results = {sum: 0, rolls: []};
+    while (i < n) {
+        let res = dice();
+        results.sum += res;
+        results.rolls.push(res);
+        i++;
+    }
+
+    return results;
+}
+
+console.log("\nExercise 8");
+console.log(rollTheDices(5));
+
+/* EXERCISE 9
+
+Write a function called howManyDays which receives a date as a parameter and returns the number of days passed since that date.
+
+*/
+
+//this was in the extras yesterday
+function howManyDays(date) {
+    let today = new Date();
+    let dateDiffms = today - date;
+    
+    return Math.floor(dateDiffms / (1000 * 60 * 60 * 24));
+}
+
+console.log("\nExercise 9");
+console.log(howManyDays(new Date()));
+console.log(howManyDays(new Date(2022, 11,1)));
+console.log(howManyDays(new Date(2022, 10,2)));
+
+/* EXERCISE 10
+
+Write a function called isTodayMyBirthday which should return true if today’s your birthday, false otherwise.
+
+*/
+
+// I chose to give the function an input to make it more flexible rather than just check if month is may and day is 12th
+function isTodayMyBirthday(someDate) {
+    let today = new Date();
+    return (today.getDate() === someDate.getDate()) && (today.getMonth() === someDate.getMonth());
+}
+
+console.log("\nExercise 10");
+console.log(isTodayMyBirthday(new Date(1992, 4, 12)));
+console.log(isTodayMyBirthday(new Date(1992, 11, 2)));
+
+// JS Arrays & Objects
+
+// NOTE: the movies array used in some exercises is defined at the end of this file
+
+/* EXERCISE 11
+
+Write a function called deleteProp which receives an object and a string as parameters,
+
+and returns the given object after deleting its property named as the given string.
+
+*/
+
+function deleteProp(obj, str) {
+    delete obj[str];
+    return obj;
+}
+
+console.log("\nExercise 11");
+console.log(deleteProp(movies[0], "Poster"));
+
+
+/* EXERCISE 12
+
+Write a function called oldestMovie which finds the oldest movie in the provided movies array.
+
+*/
+
+function oldestMovie() {
+    let oldest = movies[0];
+    let releaseDate = parseInt(oldest.Year);
+
+    for (let i = 1; i < movies.length; i++) {
+        if (parseInt(movies[i].Year) < releaseDate) {
+            oldest = movies[i];
+            releaseDate = parseInt(movies[i].Year);
+        }
+    }
+
+    return oldest;
+}
+
+console.log("\nExercise 12");
+console.log(oldestMovie());
+
+
+/* EXERCISE 13
+
+Write a function called countMovies which returns the number of movies contained in the provided movies array.
+
+*/
+
+function countMovies() {
+    return movies.length;
+}
+
+console.log("\nExercise 13");
+console.log(countMovies());
+
+/* EXERCISE 14
+
+Write a function called onlyTheTitles which creates an array with just the titles of the movies contained in the provided movies array.
+
+*/
+
+function onlyTheTitles() {
+    let titles = [];
+    for (let i = 0; i < movies.length; i++) {
+        titles.push(movies[i].Title);
+    }
+
+    return titles;
+}
+
+console.log("\nExercise 14");
+console.log(onlyTheTitles());
+
+/* EXERCISE 15
+
+Write a function called onlyInThisMillennium which returns only the movies produced in this millennium from the provided movies array.
+
+*/
+
+function onlyInThisMillennium() {
+    let titles = [];
+    for (let i = 0; i < movies.length; i++) {
+        if (parseInt(movies[i].Year) >= 2000) {
+            titles.push(movies[i]);
+        }
+    }
+
+    return titles;
+}
+
+console.log("\nExercise 15");
+console.log(onlyInThisMillennium());
+
+/* EXERCISE 16
+
+Write a function called getMovieById which receives an id as a parameter and returns the movie with the given id from the provided movies array.
+
+*/
+
+function getMovieById(id) {
+    let movie = null; //if no match
+    let i = 0;
+    while (movie === null && i < movies.length) {
+        let idToComp = movies[i].imdbID;
+        if (id.toLocaleLowerCase() === idToComp.toLocaleLowerCase()) {
+            movie = movies[i];
+        };
+        i++;
+    }
+
+    return movie !== null ? movie : "No result";
+}
+
+console.log("\nExercise 16");
+console.log(getMovieById("tt0087365"));
+console.log(getMovieById("eins"));
+
+/* EXERCISE 17
+
+Write a function called sumAllTheYears which returns the sum of all the years in which the movies in the provided movies array have been produced.
+
+*/
+
+//why
+function sumAllTheYears() {
+    let sum = 0;
+    for (let i = 0; i < movies.length; i++) {
+        sum += parseInt(movies[i].Year);
+    };
+    return sum;
+}
+console.log("\nExercise 17");
+console.log(sumAllTheYears());
+
+/* EXERCISE 18
+
+Write a function called searchByTitle which receives a string as a parameter and returns all the movies in the provided movies array which contain that string in the title.
+
+*/
+
+//Doing this backwards I only now realized I could've searched the titlesArray from earlier because the titles there have the same index as the corresponding movies in the movies Array
+function searchByTitle(str) {
+    let result = [];
+    for (let i = 0; i < movies.length; i++) {
+        if (movies[i].Title.toLocaleLowerCase().includes(str.toLocaleLowerCase())) {
+            result.push(movies[i]);
+        } 
+    };
+    return result;
+}
+console.log("\nExercise 18");
+console.log(searchByTitle("avengers"));
+
+
+/* EXERCISE 19
+
+Write a function called searchAndDivide which receives a string as a parameter and returns an object;
+
+this object should contain an array called match, made by all the movies from the provided movies array which contain the given string in the title,
+
+and another array unmatch with all the remaining ones.
+
+*/
+
+// is case sensitive so far, you could set movies[i].Title.toLowerCase() and str.toLowerCase() as well
+function searchAndDivide(str) {
+    let result = {match: [], unmatch: []}
+    for (let i = 0; i < movies.length; i++) {
+        if (movies[i].Title.toLocaleLowerCase().includes(str.toLocaleLowerCase())) {
+            result.match.push(movies[i]);
+        } else {
+            result.unmatch.push(movies[i]);
+        };
+    };
+    return result;
+}
+
+console.log("\nExercise 19");
+console.log(searchAndDivide("Lord"), searchAndDivide("Lord").match.length, searchAndDivide("Lord").unmatch.length);
+
+/* EXERCISE 20
+
+Write a function called "removeIndex" which receives a number as a parameter and returns the provided movies array without the element in the given position.
+
+*/
+
+function removeIndex(n) {
+    return movies.splice(n, 1);
+}
+
+console.log("\nExercise 20");
+console.log(movies.length);
+console.log(removeIndex(3));
+console.log(movies.length);
+
+// [EXTRAS] JS Advanced
+
+/* EXERCISE 21
+
+Create a function called "halfTree" which receives a number as a parameter and builds an "*" half tree with the given height.
+
+Example:
+
+halfTree(3)
+
+*
+
+**
+
+***
+
+*/
+
+function halfTree(n) {
+    let i = 1;
+    let tree = "";
+    while (i <= n) {
+        tree = tree.concat("*".repeat(i));
+        tree = tree.concat("\n");
+        i++;
+    }
+    return tree;
+}
+
+console.log("\nExercise 21");
+console.log(halfTree(3));
+
+
+/* EXERCISE 22
+
+Create a function called "tree" which receives a number as a parameter and builds an "*" tree with the given height.
+
+Example:
+
+tree(3)
+
+*
+
+***
+
+*****
+
+*/
+
+// Honestly I think there is a better way to declare i and to maybe not build the tree backwards but this is what i came up with first so i will only fix this if i have the time
+function tree(n) {
+    let i = n;
+    let tree = "";
+    let maxWidth = 2 * (n-1) + 1
+    while (i > 0) {
+        let row = "*".repeat(1 + 2 * (i - 1));
+        let space = " ".repeat((maxWidth - row.length) / 2);
+        tree = (space + row + space).concat(tree);
+        tree = "\n".concat(tree);
+        i--;
+    }
+    return tree;
+}
+
+console.log("\nExercise 22");
+console.log(tree(3));
+
+/* EXERCISE 23
+
+*/
+
+function isItPrime(n) {
+    let isIt = true;
+    if (n !== 1) {
+        for (let i = 2; i < n; i++) {
+            if (n % i === 0) {
+                isIt = false;
+            };
+        };
+    };
+    return isIt;
+};
+
+console.log("\nExercise 23");
+console.log(isItPrime(1))
+console.log(isItPrime(2))
+console.log(isItPrime(23))
+console.log(isItPrime(42))
